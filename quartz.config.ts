c0f7577f1e6e21094088c837a013b4a0a0b01e9a
Keith McCarron <kmccarro@us.ibm.com>
2026-04-22 08:46:56 -0400

    fix and personalize base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "fractalk.github.io/ai-auto-wiki",
     ignorePatterns: [
       "private",
       "templates",
